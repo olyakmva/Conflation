dd3c1b98e70f23af97c34c7156abc330fa2d4a24
Vladislav Gorshkov <gorshkov200168@gmail.com>
2024-02-23 17:06:46 +0300

    Adding FrequencyAnalysisLib project and UnitTests for him

---
+++ KeyPointApp/FrequencyAnalysisLib/FreqAlg.cs
@@ -0,0 +1,21 @@
+﻿using SupportLib;
+using System.Transactions;
+
+namespace FrequencyAnalysisLib
+{
+    public class FreqAlg
+    {
+        private Map curr_map;
+        public FreqAlg(Map map)
+        {
+            curr_map = map;
+        }
+        public Map Curr_map 
+        { 
+            get 
+            { 
+                return curr_map;
+            } 
+        }
+    }
+}
---
+++ KeyPointApp/FrequencyAnalysisLibTestProject/FreqAlgTest.cs
@@ -0,0 +1,24 @@
+using FrequencyAnalysisLib;
+using SupportLib;
+using System.Data;
+
+namespace FrequencyAnalysisLibTestProject
+{
+    public class FreqAlgTest
+    {
+        [Fact]
+        public void FreqAlgNotNullAfterCreating()
+        {
+            Map map = new Map();
+            FreqAlg freqAlg = new FreqAlg(map);
+            Assert.NotNull(freqAlg);
+        }
+        [Fact]
+        public void FreqAlgContainsMapAfterCreating()
+        {
+            Map map = new Map();
+            FreqAlg freqAlg = new FreqAlg(map);
+            Assert.Equal(freqAlg.Curr_map, map);
+        }
+    }
+}
